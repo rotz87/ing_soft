1ad7d79e5f85ec93c2743777879f3b329fb7c26c
MattMic87 <mattia.micozzi@msn.com>
2014-12-18 13:52:41 +0100

    aggiunto il template per l'elenco degli appelli

--- RegistroScolastico/WebContent/site/classi/js/app.js
+++ RegistroScolastico/WebContent/site/classi/js/app.js
@@ -2,3 +2,7 @@ var appelloApp = angular.module('appelloApp',[
                                               'appelloControllers',
                                               'appelloServices'
                                               ]);
+appelloApp.value('$sniffer', { history: false });
+appelloApp.config(function($locationProvider) {
+  $locationProvider.html5Mode(false).hashPrefix('');
+})
--- RegistroScolastico/WebContent/site/classi/js/controllers.js
+++ RegistroScolastico/WebContent/site/classi/js/controllers.js
@@ -1,6 +1,6 @@
 var appelloControllers =  angular.module('appelloControllers',[]);
 
-appelloControllers.controller('avviaAppello',['$scope','Appello',function($scope,Appello){
+appelloControllers.controller('avviaAppello',['$scope','Appello','$location',function($scope,Appello,$location){
 		$scope.idClasse = 0;
 		$scope.creaAppello = function(miaClasse){
 			if (miaClasse)
@@ -8,10 +8,97 @@ appelloControllers.controller('avviaAppello',['$scope','Appello',function($scope
 				$scope.idClasse = miaClasse;
 			}
 			
-			Appello.myPost({idClasse:$scope.idClasse},null,function(data,header){
+			Appello.myPost({idClasse:$scope.idClasse},function(data,header){
 				console.log(data);
 				console.log(header());
+				$scope.appelloUri = header("location");
+				
 			});
+			var myuri = "appelli";
 			
+			$location.path("appelli/");
+			console.log($location.protocol());
+			console.log($location.href);
+			console.log($location.host());
+			console.log($location.port());
+			console.log($location.hash());
+			
+			console.log($location.absUrl());
+			console.log($location.hash());
 		}
+		
 	}]);
+
+appelloControllers.controller('riempiAppelli', ['$scope', function($scope) {
+  $scope.appello1 = { data: 'Naomi', azione: 'visualizza' };
+  $scope.appello2 = { data: 'Igor', azione: 'Avvia' };
+}])
+.directive('mioAppello', function() {
+  return {
+	restrict : 'E',
+	transclude:true,
+    scope: {
+      appelloInfo: '=info'
+    },
+    templateUrl: 'partials/appelli.html'
+  };
+});
+appelloControllers.controller('riempiElencoAppelli', ['$scope', function($scope) {
+	  $scope.appello1 = { data: 'Naomi', azione: 'visualizza' };
+	  $scope.appello2 = { data: 'Igor', azione: 'Avvia' };
+	  $scope.mioController = "avviaAppello";
+	  $scope.elencoAppelli = [{data:'naomi',azione:'visualizza',esiste:"true"},
+	                          {data:'peppe',azione:'Avvia',esiste:"false"},
+	                          {data:'giu',azione:'Avvia',esiste:"false"},
+	                          {data:'pic',azione:'Avvia',esiste:"false"},
+	                          {data:'tiz',azione:'Avvia',esiste:"false"}
+	                          ];
+	  $scope.mioController = {nome:"ng-controller",azione:"creaAppello(0)"};
+	  var creaAppello = false;
+	  for(var i=0;i<$scope.elencoAppelli.length;i++)
+		  {
+		  	if($scope.elencoAppelli[i].esiste == "true"){
+		  		$scope.elencoAppelli[i].click = "";
+		  	}
+		  	else if ($scope.elencoAppelli[i].esiste == "false" && creaAppello == false)
+		  	{
+		  		creaAppello = true;
+		  		$scope.elencoAppelli[i].click = "creaAppello(0)";
+		  		
+		  	}
+		  	else
+		  	{
+		  		$scope.elencoAppelli[i].click = "";
+		  	}
+		  }
+	  /*
+	   * {
+            'id': '0',
+            'data': '1288323623006',
+            'studenti': [
+                {
+                    'idStudente': '00000001',
+                    'nome': 'Maccio',
+                    'cognome': 'Capatonda'
+                },
+                {
+                    'idStudente': '00000002',
+                    'nome': 'Maccio2',
+                    'cognome': 'Capatonda2'
+                },
+                {
+                    'idStudente': '00000003',
+                    'nome': 'Maccio3',
+                    'cognome': 'Capatonda3'
+                }
+            ]
+        }
+	   * */
+	}])
+	.directive('elencoAppello', function() {
+	  return {
+		restrict : 'E',
+		transclude:true,
+	    templateUrl: 'partials/elencoAppelli.html'
+	  };
+	});
