3e149aabe276ccd4e94b0725107631c2b5cc9438
marco <marco@marco-Q143F>
2015-01-15 17:37:57 +0100

    Il server restituisce le assenze di un appello

---
+++ RegistroScolastico/src/resourceSupport/AssenzaAppelloRS.java
@@ -0,0 +1,23 @@
+package resourceSupport;
+
+import org.springframework.hateoas.ResourceSupport;
+
+import domain.Assenza;
+
+public class AssenzaAppelloRS extends ResourceSupport {
+	
+	private long idAssenza;
+
+	public AssenzaAppelloRS(Assenza assenza) {
+		setIdAssenza(/*assenza.getId()*/0L);
+	}
+
+	public long getIdAssenza() {
+		return idAssenza;
+	}
+
+	public void setIdAssenza(long idAssenza) {
+		this.idAssenza = idAssenza;
+	}
+	
+}
