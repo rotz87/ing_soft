ce66a5d8abe4aae0594a06288b4f22f7c89afd85
Lorenzo Di Maio <lolly.dimaio@gmail.com>
2015-01-22 12:57:33 +0100

    TestFaiLAppelloController

--- RegistroScolastico/src/test/TestFaiLAppelloController1.java
+++ RegistroScolastico/src/test/TestFaiLAppelloController1.java
@@ -1,20 +1,15 @@
 package test;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
 import java.util.Map.Entry;
 
-import org.joda.time.LocalDate;
-
-import controllerDomain.FaiAppelloController;
 import service.DBFake;
-import domain.*;
+import controllerDomain.FaiAppelloController;
+import domain.Appello;
+import domain.Assenza;
+import domain.LibrettoAssenze;
+import domain.RegistroAssenze;
+import domain.Studente;
 
 public class TestFaiLAppelloController1 {
 	public static void main(String[] args){
