72971db209368d171fd6837ebce85663a36c819e
MattMic87 <mattia.micozzi@gmail.com>
2015-05-11 11:25:46 +0200

    cambiato l'url base da /site/classi/ a /site/

---
+++ RegistroScolastico/WebContent/site/js/app.js
@@ -0,0 +1,77 @@
+var registroApp = angular.module('registroApp',[
+                                              'registroControllers',
+                                              'registroServices',
+                                              'registroFilters',
+                                              'ngRoute',
+                                              'ngAnimate',
+                                              'checklist-model'
+                                              ]);
+registroApp.config(["$locationProvider","$routeProvider",function($locationProvider,$routeProvider) 
+{
+  $routeProvider
+  .when('/impostazioni/',
+  {
+	  templateUrl:'partials/impostazioni.html',
+	  controller:'impostazioniController',
+	  title:'impostazioni'
+  }).when('/classi/:idClasse/registroDiClasse/appelli/:idAppello/',{
+	  templateUrl: 'partials/appelloParziale.html',
+	  controller: 'faiAppello',
+	  title:'Fai l\'appello'
+  }).when('/classi/:idClasse/registroDiClasse/',{
+	  templateUrl: 'partials/elencoAppelli.html',
+	  controller: 'riempiElencoAppelli',
+	  title:'Seleziona l\'appello'
+  }).when('/classi/:idClasse/',{
+	  templateUrl: 'partials/elencoRegistri.html',
+	  controller: 'elencoRegistri',
+	  title:'Seleziona il registro'
+  }).when('/classi/:idClasse/appelli/',{
+	  redirectTo:function(routeParams){
+		  return "/"+routeParams.idClasse+'/';
+	  }
+  })
+//  	.when('/:idClasse/registroDocente/',{
+//	  templateUrl: 'partials/compito.html',
+//	  controller: 'elencoMaterie',
+//	  title:'Seleziona il registro Docente'
+//  })
+//  	.when('/:idClasse/registriDocente/:idRegistroDocente',{
+//	  templateUrl: 'partials/compito.html',
+//	  controller: '',
+//	  title:'Seleziona l\'operazione'
+//  })
+  	.when('/classi/:idClasse/registriDocente/:idRegistroDocente/',
+  {
+	  templateUrl:'partials/registroDocente.html',
+	  controller:'funzioniRegistroDocente',
+	  title:'Registro Docente'
+  }).when('/classi/:idClasse/registriDocente/:idRegistroDocente/compiti/',{
+	  templateUrl: 'partials/elencoCompiti.html',
+	  controller: 'riempiElencoCompiti',
+	  title:'Elenco dei compiti'
+  }).when('/classi/:idClasse/registriDocente/:idRegistroDocente/compiti/:idCompito/',{
+	  templateUrl: 'partials/compito.html',
+	  controller: 'recuperaCompitoInClasse',
+	  title:'Imposta il compito'
+  }).when('/classi/:idClasse/registriDocente/:idRegistroDocente/mediaVoti/',
+  {
+	  templateUrl:'partials/mediaVoti.html',
+	  controller:'mediaVotiController',
+	  title:'Calcola la media dei voti'
+  }).when('/classi',
+  {
+	  templateUrl:'partials/elencoClassi.html',
+	  controller:'riempiElencoClassi',
+	  title:'seleziona la classe'
+  }).when('/',{
+	  /*
+	   * la pagina iniziale riporta automaticamente 
+	   * all'elenco delle classi
+	   * */
+	  redirectTo:function(routeParams){
+		  return "/classi/";
+	  }
+  })
+  //$locationProvider.html5Mode({enabled:true,requireBase:true});
+}]);
